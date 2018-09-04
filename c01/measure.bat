29e0ae7c8579233e617d6beb30b7cb5834737b6e
hmjo226 <42786090+hmjo226@users.noreply.github.com>
2018-09-04 17:43:00 -0400

    ogr2ogr

---
+++ c01/measure.bat
@@ -0,0 +1,6 @@
+ogr2ogr --version
+ogrinfo -so -al C:\JohnsonGIS\data\ne_10m_admin_1_states_provinces_lakes\ne_10m_admin_1_states_provinces_lakes.shp
+
+ogr2ogr -f geojson projected.geojson -sql "select name from ne_10m_admin_1_states_provinces_lakes where name in ('Texas','Alaska','Kentucky')" -s_srs EPSG:4326 -t_srs EPSG:5070 C:\JohnsonGIS\data\ne_10m_admin_1_states_provinces_lakes\ne_10m_admin_1_states_provinces_lakes.shp
+
+ogr2ogr -f CSV output.csv -sql "select name, (OGR_GEOM_AREA/1000000) as sq_km from ne_10m_admin_1_states_provinces_lakes" projected.geojson
